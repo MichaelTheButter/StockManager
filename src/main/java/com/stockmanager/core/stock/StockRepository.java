8915b6310d1d17a3d4b1a043f8eaf6df04e2e656
MichaelTheButter <136842400+MichaelTheButter@users.noreply.github.com>
2024-01-21 14:29:21 +0100

    Delete src/main/java/com/stockmanager/core/stock directory
    restart

--- src/main/java/com/stockmanager/core/stock/StockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stockmanager.core.stock;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.repository.CrudRepository;
-
-@Repository
-public interface StockRepository extends CrudRepository<Stock, Long>{
-}
