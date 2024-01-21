094e777946bb81c6f46640a0f1e2316415c75aea
MichaelTheButter <136842400+MichaelTheButter@users.noreply.github.com>
2024-01-21 14:29:09 +0100

    Delete src/main/java/com/stockmanager/core/product directory
    restart

--- src/main/java/com/stockmanager/core/product/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stockmanager.core.product;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
-}
