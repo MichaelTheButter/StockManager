07b1429f763e069eca5b8eb3441103b2a3884f2a
michal <michalmaselko@wp.pl>
2024-01-30 09:03:52 +0100

    add stock test

--- src/test/java/com/stockmanager/infrastructure/controllers/StockControllerTest.java
+++ src/test/java/com/stockmanager/infrastructure/controllers/StockControllerTest.java
@@ -2,27 +2,23 @@ package com.stockmanager.infrastructure.controllers;
 
 import com.stockmanager.core.stock.StockService;
 import com.stockmanager.core.stock.dto.StockDto;
-import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,18 +30,29 @@ public class StockControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     private StockService stockService;
     @Test
     public void shouldCreateNewStock() throws Exception {
+        // given
         String stock = """ 
                 {"name": "testStock"}
                 """;
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/stocks")
+        // when
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/stocks")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(stock)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().is(201))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(2));
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(201))
+                .andReturn();
+        // then
+        StockDto stockDto = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), StockDto.class);
+        assertNotNull(stockDto);
+        assertEquals(2L, stockDto.getId());
+        assertEquals("testStock", stockDto.getName());
+
     }
 
     @Test
